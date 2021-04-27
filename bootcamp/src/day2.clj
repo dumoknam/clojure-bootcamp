e87a378bca84a718bdf106917e154848947d80ed
sh.nam <sh.nam@greenlabs.co.kr>
2021-04-27 11:52:18 +0900

    AoC 2020 Day 2 refactoring

--- bootcamp/src/day2.clj
+++ bootcamp/src/day2.clj
@@ -7,46 +7,49 @@
                (clojure.string/split-lines)))
 
 ; part 1.
-(defn frequency-ch-in-str [ch str]
-  (get (frequencies str) (first ch) 0))
+(defn frequency
+  [v m]
+  (get (frequencies m) v 0))
 
 (defn valid-password? [{i :i j :j ch :ch password :password}]
-  (and (<= (frequency-ch-in-str ch (seq password)) (Integer/parseInt j))
-       (>= (frequency-ch-in-str ch (seq password)) (Integer/parseInt i))))
+  (let [freq (frequency ch password)]
+    (<= i freq j)))
 
 (def line-regex #"(\d+)-(\d+) (\w{1}): (\w+)")
 
 (defn input-map [line]
-  (zipmap [:i :j :ch :password]
-          (rest (re-find line-regex line))))
+  (let [[_ i j ch password] (re-find line-regex line)]
+    {:i        (Integer/parseInt i)
+     :j        (Integer/parseInt j)
+     :ch       (first ch)
+     :password password}))
 
 ; part 1 answer
 (->> input
      (map input-map)
-     (map valid-password?)
-     (filter true?)
+     (filter valid-password?)
      (count))
 
 ; part 2.
 ; 글자 두개 꺼내서 한 번만 있는지 확인
-(defn character-frequencies [{i :i j :j ch :ch password :password}]
-  (frequency-ch-in-str
-    ch
-    [(get password (dec (Integer/parseInt i))) (get password (dec (Integer/parseInt j)))]))
+;   [{i :i j :j ch :ch password :password}] 이건
+;== [{:keys [i j ch password]}] 와 같다.
+(defn valid-password2? [{i :i j :j ch :ch password :password}]
+  (let [freq (frequency ch [(get password (dec i)) (get password (dec j))])]
+    (= freq 1)))
 
 ; part 2 answer
 (->> input
      (map input-map)
-     (map character-frequencies)
-     (filter #(= 1 %))
+     (filter valid-password2?)
      (count))
 
 ; ----------
 (comment
   (get-two-ch 1 2 "abcde")
   (pr input)
-  (frequencies (seq "xpxc"))
-  (frequency-ch-in-str \x "xpxc")
+  (frequencies "xpxc")
+  (frequency \x "xpxc")
   (valid-password? {:i "3" :j "5" :ch "v" :password "qvjvjdhvl"})
   (parse-input "1-2 x: xpxc")
   (input-map ["1-2 x: xpxc"])
